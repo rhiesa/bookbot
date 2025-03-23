28b0a4c7d1ad1323f24bfab77df8980c1c96dbf1
Rhiesa <aaron.r.boyd@gmail.com>
2025-03-23 02:01:20 -0600

    frankenstein temp

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(filepath):
+	with open(filepath) as f:
+		file_contents = f.read()
+	return file_contents
+def main():
+	get_book_text("./books/frankenstein.txt")	
+main()
